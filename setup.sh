2b91756682aa691d2ae3ce13c40070396a363a0f
Rutger Hartog <rhartog@bol.com>
2022-03-10 08:54:54 +0100

    changed load order

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ cat > /etc/nginx/http.d/default.conf <<EOF
 server {
     listen 8000;
     root /data;
-    index index.html;
 
     location / {
         dav_methods PUT DELETE MKCOL COPY MOVE;
@@ -36,15 +35,8 @@ http {
         client_max_body_size 1m;
         sendfile on;
         tcp_nopush on;
-        #gzip on;
+        gzip on;
         gzip_vary on;
-        log_format json_combined escape=json 
-            '{'
-                '"http": {'
-                    '"time": "$time_local"' 
-                '}'
-            '}';
-        access_log /dev/stdout json_combined;
         include /etc/nginx/http.d/*.conf;
 }
 EOF
